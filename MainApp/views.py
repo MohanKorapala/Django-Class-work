fd0e495a83350452312b3f1f91813fa71192b484
Mohan Korapala <mohan_korapala1@baylor.edu>
2022-05-02 15:46:53 -0500

    Class work 5/2

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from MainApp.forms import TopicForm
-from .models import Topic
+from .forms import TopicForm, EntryForm
+from .models import Entry, Topic
 from .forms import Topic
 
 # Create your views here.
@@ -32,7 +32,7 @@ def new_topic(request):
         form = TopicForm(data=request.POST)
 
         if form.is_valid():
-            new_tpoic = form.save()
+            new_topic = form.save()
 
 
             return redirect('MainApp:topics')
@@ -40,4 +40,37 @@ def new_topic(request):
     context = {'form':form}
     return render(request,'MainApp/new_topic.html', context)
 
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('MainApp:topic',topic_id=topic_id)
+
+    context = {'form':form,'topic':topic}
+    return render(request,'MainApp/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('MainApp:topic',topic_id=topic.id)
+
+    context = {'form':form,'topic':topic, 'entry':entry}
+    return render(request,'MainApp/edit_entry.html', context)
 
